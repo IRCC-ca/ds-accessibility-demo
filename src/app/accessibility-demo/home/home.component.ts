07685e73aa2053c36bf47e2e2a4c5fa4559b67cb
Bobby Brice <bobbybrice@gmail.com>
2023-09-26 14:51:37 -0400

    add home page navigation fn

--- src/app/accessibility-demo/home/home.component.ts
+++ src/app/accessibility-demo/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IButtonConfig } from 'ircc-ds-angular-component-library';
-
+import { Router } from '@angular/router';
+import { LanguageSwitchService } from '@app/@shared/language-switch/language-switch.service';
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,13 +14,25 @@ export class HomeComponent implements OnInit {
     size: 'large',
   };
 
-  constructor() { }
+  constructor(private router: Router,
+    private translate: TranslateService,
+    private altLang: LanguageSwitchService,) { }
 
   ngOnInit(): void {
   }
 
   nextPage() {
-    
+    this.router.navigateByUrl(this.getMainPageLink);
+  }
+
+  get getMainPageLink() {
+    const curLang = this.translate.currentLang;
+    this.translate.use((curLang === 'en-US') || (curLang === 'en') ? 'en-US' : 'fr-FR');
+    const lang = ((curLang === 'en-US') || (curLang === 'en') ? 'en' : 'fr');
+    console.log('327', curLang, ('/' + lang + '/' + this.translate.instant('ROUTES.AccessibilityDemo')))
+
+    // return ''
+    return ('/' + lang + '/' + this.translate.instant('ROUTES.PersonalInfo'));
   }
 
 }
