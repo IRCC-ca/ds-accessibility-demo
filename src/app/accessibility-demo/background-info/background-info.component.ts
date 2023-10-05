84982d38bec57f266216aacb6c9285ba44b9671b
Bobby Brice <bobbybrice@gmail.com>
2023-10-05 15:08:56 -0400

    remove duplicate code

--- src/app/accessibility-demo/background-info/background-info.component.ts
+++ src/app/accessibility-demo/background-info/background-info.component.ts
@@ -109,68 +109,6 @@ export class BackgroundInfoComponent implements OnInit {
     }
   }
 
-  /*************** LANGUAGE FUNCTIONS ********************/
-
-  /** Toggles language without reloading component */
-  //This currently uses both 'en' and 'en-US' language values, sine in some cases, en is provided in initial load
-  changeLang() {
-    const curLang = this.translate.currentLang;
-    this.translate.use(
-      curLang === 'en-US' || curLang === 'en' ? 'fr-FR' : 'en-US'
-    );
-    // Changes the html lang attribute
-    // console.log((curLang === "en-US") || (curLang === 'en') ? 'fr' : 'en');
-    document.documentElement.lang =
-      curLang === 'en-US' || curLang === 'en' ? 'fr' : 'en';
-    // Pushes page into history to allow the use of the 'Back' button on browser
-    window.history.pushState('', '', this.altLangURL);
-    this.setAltLangURL();
-    console.log(this.altLangURL, this.altPathKey);
-  }
-
-  //Alt-language url key must be in the corresponding language, but have the french work
-  setAltLangURL() {
-    console.log(this.translate.currentLang);
-    this.altLangURL =
-      this.translate.currentLang === 'en-US' ||
-        this.translate.currentLang === 'en'
-        ? 'fr'
-        : 'en';
-    this.getAltLanguageValues();
-
-    if (this.altPathKey)
-      this.altLangURL +=
-        '/' + this.translate.instant('ROUTES.' + this.altPathKey);
-  }
-
-  /**
-   * Generates an alt-language path based on the current url and the translate values. Currently not the best
-   * code in the world and should likely be refactored.
-   */
-  getAltLanguageValues() {
-    const urlParts = this.router.url.split('/');
-    const translateIndex = Object.keys(this.translate.translations).indexOf(
-      urlParts[1]
-    );
-    const translateValues = (
-      Object.values(this.translate.translations)[translateIndex] as any
-    ).ROUTES;
-    const translatedURLPieces: string[] = [];
-    urlParts.forEach((val: string, index: number) => {
-      if (index > 1) {
-        const i = Object.values(translateValues as any).indexOf(val);
-        translatedURLPieces.push(Object.keys(translateValues as any)[i]);
-      }
-    });
-    translatedURLPieces.forEach((piece) => {
-      //Operates on the assumption that the alt route is the same as the route, but with '-alt' appended
-      const k = this.translate.instant('ROUTES.' + piece + '-alt');
-      if (this.translate.instant('ROUTES.' + this.altPathKey) !== k) {
-        this.altLangURL += '/' + k;
-      }
-    });
-  }
-
   ngOnDestroy() {
     this.routerSub?.unsubscribe();
     this.progressIndicatorSub?.unsubscribe();
